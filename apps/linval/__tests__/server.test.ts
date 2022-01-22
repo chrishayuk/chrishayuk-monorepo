864460c2bd06cb1d119d4b8f7f4f9d7f7b278f15
chrishayuk <chrishayuk@googlemail.com>
2022-01-22 16:36:20 +0000

    fix: fixed up lint-staged to support typescript

--- apps/linval/__tests__/server.test.ts
+++ apps/linval/__tests__/server.test.ts
@@ -1,13 +1,12 @@
-// import supertest from "supertest";
-// import { createServer } from "../src/server";
+//import supertest from "supertest";
+//import { createServer } from "../src/server";
 
 describe("server", () => {
   it("root endpoint - linval says hello", async () => {
-    //   await supertest(createServer())
-    //     .get("/")
-    //     .set('Accept', 'text/plain')  
-    //     .expect(200)
-    //     .expect("hello, i'm Linval, i'm a very slow but happy image server");
-    // });
+    // await supertest(createServer())
+    //   .get("/")
+    //   .set('Accept', 'text/plain')
+    //   .expect(200)
+    //   .expect("hello, i'm Linval, i'm a very slow but happy image server");
   });
 });
